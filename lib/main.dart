651058ecbba1fc5b48e2eeafcb4e300da750f1e0
Sachin Kumar <mail.sachin161@gmail.com>
2021-06-01 18:33:19 +0530

    minor fixes

--- lib/main.dart
+++ lib/main.dart
@@ -142,7 +142,7 @@ class _MyAppState extends State<MyApp> {
                                     borderRadius: BorderRadius.circular(10.0),
                                     side: BorderSide(color: Colors.red)))),
                     onPressed: () {
-                      if (_formKey.currentState.validate()) {
+                      if (!(datee == null)) {
                         Navigator.push(
                             context,
                             MaterialPageRoute(
