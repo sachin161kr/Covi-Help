b6c01c213c91483f65056055170a569c53e1f253
Sachin Kumar <mail.sachin161@gmail.com>
2021-06-01 14:42:08 +0530

    dropdown added

--- lib/certificate.dart
+++ lib/certificate.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:covidapp/statedata.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class Certificate extends StatefulWidget {
   Certificate({Key key}) : super(key: key);
@@ -9,9 +12,39 @@ class Certificate extends StatefulWidget {
 }
 
 class _CertificateState extends State<Certificate> {
-  final _formKey1 = GlobalKey<FormState>();
   // final _formKey2 = GlobalKey<FormState>();
-  TextEditingController myController = TextEditingController();
+
+  final String url = 'https://api.rootnet.in/covid19-in/stats/latest';
+  int len;
+
+  List<String> list = [];
+
+  bool okay = false;
+
+  var json;
+  getData() async {
+    http.Response response = await http.get(Uri.parse(url));
+    json = jsonDecode(response.body);
+
+    len = json['data']['regional'].length;
+
+    for (int i = 0; i < len; i++) {
+      list.add(json['data']['regional'][i]['loc']);
+    }
+
+    setState(() {});
+    okay = true;
+
+    print(list);
+  }
+
+  @override
+  void initState() {
+    getData();
+    super.initState();
+  }
+
+  String chosenValue;
 
   @override
   Widget build(BuildContext context) {
@@ -20,81 +53,103 @@ class _CertificateState extends State<Certificate> {
         title: Text('State-Wise Data'),
         backgroundColor: Colors.red,
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(
-              height: MediaQuery.of(context).size.height / 3 - 150,
-            ),
-            Container(
-              color: Colors.yellow,
-              padding: EdgeInsets.all(8),
-              child: Text(
-                'NOTE: Names of States are Case sensitive',
-                style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold),
-                textAlign: TextAlign.center,
-              ),
-            ),
-            Container(
-              height: 300,
-              width: 300,
-              child: Image.asset(
-                'assets/images/nurse1.png',
-                //height: double.infinity,
-                //width: double.infinity,
-                fit: BoxFit.cover,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                  top: 16, left: 30, right: 30, bottom: 15),
-              child: Form(
-                key: _formKey1,
-                child: TextFormField(
-                  controller: myController,
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return 'This Field is Mandatory';
-                    }
-                    return null;
-                  },
-                  decoration: InputDecoration(
-                    hintText: 'State Name (eg - Jharkhand)',
+      body: (list.isEmpty)
+          ? Center(
+              child: CircularProgressIndicator(
+              color: Colors.red,
+            ))
+          : SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height / 3 - 150,
                   ),
-                ),
-              ),
-            ),
-            ElevatedButton(
-              style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Colors.red),
-                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                      RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10.0),
-                          side: BorderSide(color: Colors.red)))),
-              onPressed: () {
-                if (_formKey1.currentState.validate()) {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => StateData(myController.text)));
-                }
-              },
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  'Get Data',
-                  style: TextStyle(
-                    fontSize: 25,
+                  // Container(
+                  //   color: Colors.yellow,
+                  //   padding: EdgeInsets.all(8),
+                  //   child: Text(
+                  //     'NOTE: Names of States are Case sensitive',
+                  //     style: TextStyle(
+                  //         color: Colors.black,
+                  //         fontSize: 18,
+                  //         fontWeight: FontWeight.bold),
+                  //     textAlign: TextAlign.center,
+                  //   ),
+                  // ),
+                  Container(
+                    height: 300,
+                    width: 300,
+                    child: Image.asset(
+                      'assets/images/nurse1.png',
+                      //height: double.infinity,
+                      //width: double.infinity,
+                      fit: BoxFit.cover,
+                    ),
                   ),
-                ),
+                  // Padding(
+                  //     padding: const EdgeInsets.only(
+                  //         top: 16, left: 30, right: 30, bottom: 15),
+
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        top: 8.0, left: 8, right: 8, bottom: 20),
+                    child: Container(
+                      width: double.infinity,
+                      child: Center(
+                        child: DropdownButton<String>(
+                          value: chosenValue,
+                          items: list.map<DropdownMenuItem<String>>((value) {
+                            return DropdownMenuItem<String>(
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text(
+                                    value,
+                                    style: TextStyle(
+                                      fontSize: 12,
+                                    ),
+                                  ),
+                                ),
+                                value: value);
+                          }).toList(),
+                          hint: Text('Choose the State'),
+                          onChanged: (value) {
+                            setState(() {
+                              chosenValue = value;
+                            });
+                          },
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  ElevatedButton(
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(Colors.red),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10.0),
+                                    side: BorderSide(color: Colors.red)))),
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => StateData(chosenValue)));
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        'Get Data',
+                        style: TextStyle(
+                          fontSize: 25,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
-          ],
-        ),
-      ),
     );
   }
 }
